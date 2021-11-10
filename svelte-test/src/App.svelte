70f48e7753473effbe200992e14747d0a4d15b12
Lean Miguel <lean@aukai.dev>
2021-11-10 13:46:29 +0900

    add svelte project

---
+++ svelte-test/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import "./index.css";
+  import Table from "./components/Table.svelte";
+
+  let tableVisible = false;
+</script>
+
+<div class="text-center p-4">
+  {#if !tableVisible}
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      on:click={() => {
+        tableVisible = true;
+      }}
+    >
+      Render Table in Svelte
+    </button>
+  {:else}
+    <Table />
+  {/if}
+</div>

110fd615b6ec92320d9c30a2ceae27421876d781
Lean Miguel <lean@aukai.dev>
2021-11-10 13:46:36 +0900

    add vue project

---
+++ vue-test/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded
+      "
+      v-if="!tableOpen"
+      @click="toggleTableOn"
+    >
+      Render Table with Vue
+    </button>
+    <Table v-if="tableOpen" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Table from "./components/Table.vue";
+
+const tableOpen = ref(false);
+
+const toggleTableOn = () => {
+  tableOpen.value = true;
+};
+</script>
+
+<style>
+#app {
+  padding: 16px;
+  text-align: center;
+}
+</style>
